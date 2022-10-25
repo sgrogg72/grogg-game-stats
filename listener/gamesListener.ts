9d19004051f083e3e390777a2127246ef5a54400
Stephen Grogg <sgrogg@gmail.com>
2022-10-25 15:01:32 -0400

    implement game listener

--- listener/gamesListener.ts
+++ listener/gamesListener.ts
@@ -1,23 +1,42 @@
 import schedule from 'node-schedule';
+import { fetchScheduleByDate } from '../service/nhl';
+import { GameStatusCode, ScheduleGame } from '../service/types';
 import { liveGameJob } from './liveGameListener';
 
-const jobs: any = {};
-const job = schedule.scheduleJob('*/1 * * * * *', () => {
+interface Jobs {
+  [key: string]: schedule.Job;
+}
+
+const jobs: Jobs = {};
+const job = schedule.scheduleJob('*/1 * * * * *', async () => {
   // retrieve games...
   // live games are indicated by statusCode
   // statusCode 3, 4 should be inprogress
   // for each live game create a live game job
-  const games: any = [ { id: 1, status: live }];
-  games.map((game: any) => {
-    const id = game.id;
+  const schedule = await fetchScheduleByDate(new Date());
+  const gamesLive = schedule.games.filter(g =>
+    g.gameStatusCode === GameStatusCode.inProgress ||
+    g.gameStatusCode === GameStatusCode.inProgressCritical);
+
+  const notLiveGames = schedule.games.filter(g =>
+    g.gameStatusCode !== GameStatusCode.inProgress &&
+    g.gameStatusCode !== GameStatusCode.inProgressCritical);
+
+  gamesLive.forEach((game) => {
+    const id = game.gamePk.toString();
     const foundIt = Object.keys(jobs).find((k) => k === id);
     if(!foundIt) {
       const job = liveGameJob(id);
       jobs[id] = job;
-    } else {
-      // if game isn't live anymore cancel
-      const myjob = jobs[foundIt];
-      myjob.cancel();
+    }
+  });
+
+  notLiveGames.forEach((game) => {
+    const id = game.gamePk.toString();
+    const foundIt = Object.keys(jobs).find((k) => k === id);
+    if(foundIt) {
+      jobs[foundIt].cancel();
+      delete jobs[foundIt];
     }
   });
 });
--- listener/liveGameListener.ts
+++ listener/liveGameListener.ts
@@ -1,17 +1,10 @@
 import schedule from 'node-schedule';
-import { handleRetrieveGame } from '../lambda/retrieveGame';
-import { Context, APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-ttps://statsapi.web.nhl.com/api/v1/game/2022020092/feed/live/diffPatch?startTimecode=20221025_115410
 export const liveGameJob = (id: string) => {
   return schedule.scheduleJob('*/1 * * * * *', async () => {
-    //liveData.boxscore.teams.away or home
-    // get player data
-    // for each team
-    Object.values(players);
     // statusCode 3, 4 should be inprogress
+
     // final 5, 6, 7 should be final
-    const results: APIGatewayProxyResult = await handleRetrieveGame({} as APIGatewayEvent , { functionName: 'gameStat' } as Context);
     console.log('run game job');
   });
 }
